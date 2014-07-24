8c62ad04ed6f88f79212b84cc29706f2a4b3a4e5
Your Name <itdeveloper03@gmail.com>
2014-07-24 13:24:47 +0530

    Todo Model for mongo db

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+// Todo Model
+var Todo = mongoose.model('Todo', {
+    text:String
+});
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
